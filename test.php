2e65912780438984fa6276e74c0d9bda9fab7918
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2020-08-21 21:50:46 +0000

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,37 @@
-$streamSite = "http://oxax.tv/vivid-red.html";
+<?php
+//Token
+$url1 = "https://services.iol.pt/matrix?userId=";
 
-function writeFile($url, $channel) {
-        $File = $channel.".strm";
-        $Handle = fopen($File, 'w');
-        fwrite($Handle, $url);
-        fclose($Handle);
+$options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"User-Agent: User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36&Referer=https://tviplayer.iol.pt"
+  )
+);
+
+$context = stream_context_create($options);
+$token = file_get_contents($url1, false, $context);
+
+//Link
+$url2 = file_get_contents("http://oxax.tv/vivid-red.html");
+
+preg_match_all(
+     '/((?<="videoUrl":").*?m3u8)/',
+
+    $url2,
+    $posts, // will contain the article data
+    PREG_SET_ORDER // formats data into an array of posts
+);
+
+foreach ($posts as $post) {
+    $link = $post[1];
+
 }
 
-  $input = @file_get_contents($streamSite) or die("Please try again later: $url");
+//WMS
+$wms = "?wmsAuthSign=";
 
-  preg_match("/(http.*?wmsAuthSign\=[^\&\">]+)/", $input, $matches);
-echo $matches[1];
+//Final
+header('Location:' .$link.$wms.$token);
+exit;
+?>
