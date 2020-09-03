5fa0800f29a4d63ee799edf343e3da02a79d9887
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2020-09-03 02:47:57 +0000

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+$streamSite = 'http://oxax.tv/playboy-tv.html';
+
+function writeFile($url, $channel) {
+$File = $channel.".strm";
+$Handle = fopen($File, 'w');
+fwrite($Handle, $url);
+fclose($Handle);
+}
+$input = @file_get_contents($streamSite) or die("Please try again later: $url");
+preg_match("/(http.*?wmsAuthSign\=[^\&\">]+)/", $input, $matches);
+
+echo $matches[1];
