a2be8279666f90eb6e0e9389ec37458d4a3b43ae
angahjee1994 <46360870+angahjee1994@users.noreply.github.com>
2020-12-23 00:25:41 +0000

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+{
+  "result": "ok",
+  "ads": "off",
+  "play": "on",
+  "url": "https://dutafilm.network/",
+  "app": "com.dwnovie.nontonfilm.trendingmovies",
+  "email": "waluyomania@yahoo.co.id",
+  "bannerid": "123",
+  "interid": "123"
+}
